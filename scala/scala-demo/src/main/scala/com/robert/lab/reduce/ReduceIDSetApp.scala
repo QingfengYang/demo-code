54f6b28caef9b73d510e3775c2fc1b8ff5e566bc
yangqingfeng <>
2019-02-23 20:25:49 +0800

    id reduce, fix flag issue

--- scala/scala-demo/src/main/scala/com/robert/lab/reduce/ReduceIDSetApp.scala
+++ scala/scala-demo/src/main/scala/com/robert/lab/reduce/ReduceIDSetApp.scala
@@ -7,7 +7,7 @@ import scala.io.Source
 
 object ReduceIDSetApp {
 
-  val idCounter: AtomicInteger = new AtomicInteger()
+  val idCounter: AtomicInteger = new AtomicInteger(1)
 
   def main(args: Array[String]): Unit = {
     val filename = "/Users/yangqingfeng/workspace/yqf-workspace/demo-code/scala/scala-demo/data/idmap.dat"
@@ -16,83 +16,88 @@ object ReduceIDSetApp {
     val uidGrouped: Map[String, Set[String]] = rawIdPair.groupBy(_._1).mapValues{ listIdPair: List[(String, String)] =>
       listIdPair.map(uidWf => uidWf._2).toArray.toSet
     }
-    val idMapRdd: List[IdMapSet] = uidGrouped.map{case(uid: String, wifiSet: Set[String]) =>
+    val idMapRdd: List[IdMapSet] = uidGrouped.toList.sortBy(_._1).map{case(uid: String, wifiSet: Set[String]) =>
       val setId = createSetId()
       val uidSet = Set(uid)
       IdMapSet(setId, uidSet, wifiSet)
-    }.toList
-
+    }
 
     println(idMapRdd.mkString("\n"))
     println("========")
-    reduceIDMapSet(idMapRdd)
+    val (notRelated, related) = reduceIDMapSet(idMapRdd)
+    val (notR2, related2) = reduceIDMapSet(related.get)
+    val (notR3, related3) = reduceIDMapSet(related2.get)
+    println("------------ 2www--")
+    reduceIDMapSet(related3.get)
+  }
+
+  private def mergeIdMapSetByKey(flattenDataset: List[(String, IdMapSet)]): Map[String, IdMapSet] = {
+    val groupedDataSet: Map[String, IdMapSet] = flattenDataset.groupBy(_._1).mapValues{ idMapSet_list: List[(String, IdMapSet)] =>
+      val list_IdMapSet: List[IdMapSet] = idMapSet_list.map(_._2)
+      list_IdMapSet.foldLeft(IdMapSet(Integer.MAX_VALUE, Set[String](), Set[String](), Option(Set[Int]())))((accIdMapSet, nextIdMapSet) => {
+        accIdMapSet + nextIdMapSet
+      })
+    }
+    groupedDataSet
   }
 
   def reduceIDMapSet(idMapRdd: List[IdMapSet]): (Option[List[IdMapSet]], Option[List[IdMapSet]]) = {
-    // 1. reduce by the second key
+    // 1. reduce by the wifi
     val flatten_wf_rdd: List[(String, IdMapSet)] = idMapRdd.flatten { uid_wif_set: IdMapSet =>
       uid_wif_set.wifiSet.map(wf_k => wf_k -> uid_wif_set)
     }
-    println("Flatten Wifi: \n" + flatten_wf_rdd.mkString("\n"))
-    val groupedWf: Map[String, IdMapSet] = flatten_wf_rdd.groupBy(_._1).mapValues{ idMapSet_list: List[(String, IdMapSet)] =>
-      val list_IdMapSet: List[IdMapSet] = idMapSet_list.map(_._2)
-      list_IdMapSet.foldLeft(IdMapSet(-1, Set[String](), Set[String](), Option(Set[Int]())))((accIdMapSet, nextIdMapSet) => {
-        accIdMapSet + nextIdMapSet
-      })
-    }
-    printf("1. Grouped by WiFi, size %s: \n", groupedWf.size)
+    println("1.1 Flatten Wifi: \n" + flatten_wf_rdd.mkString("\n"))
+    val groupedWf: Map[String, IdMapSet] = mergeIdMapSetByKey(flatten_wf_rdd)
+    printf("1.2 Grouped by WiFi, size %s: \n", groupedWf.size)
     printf(groupedWf.mkString("\n"))
 
+    // 2. reduce by uid
     val flatten_uid_rdd: List[(String, IdMapSet)] = groupedWf.values.flatten { uid_wf_map: IdMapSet =>
       uid_wf_map.uidSet.map(uid_k => uid_k -> uid_wf_map)
     }.toList
-    println("Flatten Wifi: \n" + flatten_uid_rdd.mkString("\n"))
-    flatten_uid_rdd.groupBy(_._1).mapValues{idMapSet_list: List[(String, IdMapSet)] =>
-      val list_IdMapSet: List[IdMapSet] = idMapSet_list.map(_._2)
-      list_IdMapSet.foldLeft(IdMapSet(-1, Set[String](), Set[String](), Option(Set[Int]())))((accIdMapSet, nextIdMapSet) => {
-        accIdMapSet + nextIdMapSet
-      })
-    }
-/*
+    println("2.1 Flatten uid: \n" + flatten_uid_rdd.mkString("\n"))
+    val groupedUid: Map[String, IdMapSet] = mergeIdMapSetByKey(flatten_uid_rdd)
+    printf("1.2 Grouped by uid, size %s: \n", groupedUid.size)
+    printf(groupedUid.toList.sortBy(_._1).mkString("\n"))
 
-    def mergeIdMapSetByKey(flatten_wf_rdd: List[(String, IdMapSet)]): Map[String, IdMapSet] = {
-      println("Flatten Wifi: \n" + flatten_wf_rdd.mkString("\n"))
-      val groupedWf: Map[String, IdMapSet] = flatten_wf_rdd.groupBy(_._1).mapValues{ idMapSet_list: List[(String, IdMapSet)] =>
-        val list_IdMapSet: List[IdMapSet] = idMapSet_list.map(_._2)
-        list_IdMapSet.foldLeft(IdMapSet(-1, Set[String](), Set[String](), Option(Set[Int]())))((accIdMapSet, nextIdMapSet) => {
-          accIdMapSet + nextIdMapSet
-        })
-      }
-    }
-*/
+    val fullJoined_idMapSet: List[IdMapSet] = groupedUid.values.toList
+    // 3. data set not relative to other set
+    val idMapSet_notRelated: List[IdMapSet] = fullJoined_idMapSet.filterNot(_.flag)
+    println("\nNoRelated: " + idMapSet_notRelated.mkString("\n"))
+    val flatten_setId_rdd: List[(String, IdMapSet)] = fullJoined_idMapSet.filter(_.flag).map(idMapSet => idMapSet.setId.toString -> idMapSet)
+    val reducedIdMapSet_list: List[(String, IdMapSet)] = mergeIdMapSetByKey(flatten_setId_rdd).toList
+    printf("3.2 Grouped by setId, size %s: \n", reducedIdMapSet_list.size)
+    printf(reducedIdMapSet_list.sortBy(_._1).mkString("\n"))
 
-    (None, None)
+    (Some(idMapSet_notRelated.map(_.resetFlag)), Some(reducedIdMapSet_list.map(_._2).map(_.resetFlag)))
   }
 
 
   def createSetId(): Int = {
     return idCounter.getAndIncrement()
   }
-
-  def createEmpth(): Unit = {
-    IdMapSet(-1, Set[String](), Set[String](), Option(Set[Int]()))
-  }
 }
 
 case class IdMapSet(setId: Int, uidSet: Set[String], wifiSet: Set[String], mergedSet: Option[Set[Int]] = None, flag: Boolean = false) {
   def + (otherIdMapSet: IdMapSet): IdMapSet = {
-    val new_setId = Math.max(setId, otherIdMapSet.setId)
+    val new_setId = Math.min(setId, otherIdMapSet.setId)
     val new_uidSet = uidSet ++: otherIdMapSet.uidSet
     val new_wfSet = wifiSet ++: otherIdMapSet.wifiSet
     val new_mergedSet: Option[Set[Int]] = mergedSet match {
       case Some(setIdSet: Set[Int]) => {
-        val tmpSetId = (setIdSet + setId + otherIdMapSet.setId).filter(_ > -1)
+        val tmpSetId = (setIdSet + setId + otherIdMapSet.setId).filter(_ < Integer.MAX_VALUE)
         Option(tmpSetId ++: otherIdMapSet.mergedSet.getOrElse(Set[Int]()))
       }
       case None => otherIdMapSet.mergedSet
     }
-    val new_flag = flag && otherIdMapSet.flag
+
+    val merged = new_uidSet.size > Math.max(uidSet.size, otherIdMapSet.uidSet.size) || new_wfSet.size > Math.max(wifiSet.size, otherIdMapSet.wifiSet.size)
+    val new_flag = flag || otherIdMapSet.flag || merged
     IdMapSet(new_setId, new_uidSet, new_wfSet, new_mergedSet, new_flag)
+  }
+
+  def resetFlag = {
+    IdMapSet(setId, uidSet, wifiSet, mergedSet)
   }
 }
 case class IdMap(uid: String, wifi: String)
