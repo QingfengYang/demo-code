d0910457dc0861a33a0803eb66fc59c78553af1c
yangqingfeng <>
2019-02-24 09:50:16 +0800

    idmap 2nd: simple reduce

--- scala/scala-demo/src/main/scala/com/robert/lab/reduce/ReduceIDSetApp.scala
+++ scala/scala-demo/src/main/scala/com/robert/lab/reduce/ReduceIDSetApp.scala
@@ -26,10 +26,10 @@ object ReduceIDSetApp {
     println(idMapRdd.mkString("\n"))
     println("========")
     val reducedIDMapDS: ArrayBuffer[IdMapSet] = ArrayBuffer.empty[IdMapSet]
-    var (notRelated_op, relatedDS_op) = reduceIDMapSet(idMapRdd)
+    var (notRelated_op, relatedDS_op) = reduceSimple(idMapRdd)
     reducedIDMapDS ++= notRelated_op.get
     while (relatedDS_op.isDefined && relatedDS_op.get.size > 1) {
-      var (notRelated_op_tmp, relatedDS_op_tmp) = reduceIDMapSet(relatedDS_op.get)
+      var (notRelated_op_tmp, relatedDS_op_tmp) = reduceSimple(relatedDS_op.get)
       relatedDS_op = relatedDS_op_tmp
       reducedIDMapDS ++= notRelated_op_tmp.get
     }
@@ -37,8 +37,26 @@ object ReduceIDSetApp {
     println("====== reduced RDD: ======\n" + reducedIDMapDS.mkString("\n"))
   }
 
+  // 第二版本
   def reduceSimple(idMapRdd: List[IdMapSet]): (Option[List[IdMapSet]], Option[List[IdMapSet]]) = {
-    (None, None)
+    // 1. reduce by the wifi
+    val flatten_wf_rdd: List[(String, IdMapSet)] = idMapRdd.flatten { uid_wif_set: IdMapSet =>
+      uid_wif_set.wifiSet.map(wf_k => wf_k -> uid_wif_set)
+    }
+    //println("1.1 Flatten Wifi: \n" + flatten_wf_rdd.mkString("\n"))
+    val groupedWf: Map[String, IdMapSet] = mergeIdMapSetByKey(flatten_wf_rdd)
+
+    val flatten_setId_rdd: List[(String, IdMapSet)] = groupedWf.values.map(idMapSet => idMapSet.setId.toString -> idMapSet).toList
+    val groupedBySetId_rdd: List[(String, IdMapSet)] = mergeIdMapSetByKey(flatten_setId_rdd).toList
+    printf("3.2 Grouped by setId, size %s: \n", groupedBySetId_rdd.size)
+    printf(groupedBySetId_rdd.sortBy(_._1).mkString("\n"))
+    val reducedIDMap_DS: List[IdMapSet] = groupedBySetId_rdd.map(_._2)
+
+    val idMapSet_notRelated: List[IdMapSet] = reducedIDMap_DS.filterNot(_.flag).map(_.resetFlag)
+    println("\nNoRelated: " + idMapSet_notRelated.mkString("\n"))
+    val related_idMapSet: List[IdMapSet] = reducedIDMap_DS.filter(_.flag).map(_.resetFlag)
+
+    (Some(idMapSet_notRelated), Some(related_idMapSet))
   }
 
   def reduceIDMapSet(idMapRdd: List[IdMapSet]): (Option[List[IdMapSet]], Option[List[IdMapSet]]) = {
