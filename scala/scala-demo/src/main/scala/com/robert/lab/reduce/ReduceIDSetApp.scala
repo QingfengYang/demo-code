674f02e97584486c7ba6b3b92bbe786627c91439
yangqingfeng <>
2019-02-24 09:44:20 +0800

    id map: 1th checked v

--- scala/scala-demo/src/main/scala/com/robert/lab/reduce/ReduceIDSetApp.scala
+++ scala/scala-demo/src/main/scala/com/robert/lab/reduce/ReduceIDSetApp.scala
@@ -3,6 +3,7 @@ package com.robert.lab.reduce
 import java.util.UUID
 import java.util.concurrent.atomic.AtomicInteger
 
+import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
 
 object ReduceIDSetApp {
@@ -24,12 +25,57 @@ object ReduceIDSetApp {
 
     println(idMapRdd.mkString("\n"))
     println("========")
-    val (notRelated, related) = reduceIDMapSet(idMapRdd)
-    val (notR2, related2) = reduceIDMapSet(related.get)
-    val (notR3, related3) = reduceIDMapSet(related2.get)
-    println("------------ 2www--")
-    reduceIDMapSet(related3.get)
+    val reducedIDMapDS: ArrayBuffer[IdMapSet] = ArrayBuffer.empty[IdMapSet]
+    var (notRelated_op, relatedDS_op) = reduceIDMapSet(idMapRdd)
+    reducedIDMapDS ++= notRelated_op.get
+    while (relatedDS_op.isDefined && relatedDS_op.get.size > 1) {
+      var (notRelated_op_tmp, relatedDS_op_tmp) = reduceIDMapSet(relatedDS_op.get)
+      relatedDS_op = relatedDS_op_tmp
+      reducedIDMapDS ++= notRelated_op_tmp.get
+    }
+    reducedIDMapDS ++= relatedDS_op.get
+    println("====== reduced RDD: ======\n" + reducedIDMapDS.mkString("\n"))
   }
+
+  def reduceSimple(idMapRdd: List[IdMapSet]): (Option[List[IdMapSet]], Option[List[IdMapSet]]) = {
+    (None, None)
+  }
+
+  def reduceIDMapSet(idMapRdd: List[IdMapSet]): (Option[List[IdMapSet]], Option[List[IdMapSet]]) = {
+    // 1. reduce by the wifi
+    val flatten_wf_rdd: List[(String, IdMapSet)] = idMapRdd.flatten { uid_wif_set: IdMapSet =>
+      uid_wif_set.wifiSet.map(wf_k => wf_k -> uid_wif_set)
+    }
+    //println("1.1 Flatten Wifi: \n" + flatten_wf_rdd.mkString("\n"))
+    val groupedWf: Map[String, IdMapSet] = mergeIdMapSetByKey(flatten_wf_rdd)
+    //printf("1.2 Grouped by WiFi, size %s: \n", groupedWf.size)
+    //printf(groupedWf.mkString("\n"))
+
+    // 2. reduce by uid
+    val flatten_uid_rdd: List[(String, IdMapSet)] = groupedWf.values.flatten { uid_wf_map: IdMapSet =>
+      uid_wf_map.uidSet.map(uid_k => uid_k -> uid_wf_map)
+    }.toList
+    //println("2.1 Flatten uid: \n" + flatten_uid_rdd.mkString("\n"))
+    val groupedUid: Map[String, IdMapSet] = mergeIdMapSetByKey(flatten_uid_rdd)
+    //printf("1.2 Grouped by uid, size %s: \n", groupedUid.size)
+    //printf(groupedUid.toList.sortBy(_._1).mkString("\n"))
+
+    val fullJoined_idMapSet: List[IdMapSet] = groupedUid.values.toList
+    // 3. data set not relative to other set
+
+    val flatten_setId_rdd: List[(String, IdMapSet)] = fullJoined_idMapSet.map(idMapSet => idMapSet.setId.toString -> idMapSet)
+    val groupedBySetId_rdd: List[(String, IdMapSet)] = mergeIdMapSetByKey(flatten_setId_rdd).toList
+    printf("3.2 Grouped by setId, size %s: \n", groupedBySetId_rdd.size)
+    printf(groupedBySetId_rdd.sortBy(_._1).mkString("\n"))
+    val reducedIDMap_DS: List[IdMapSet] = groupedBySetId_rdd.map(_._2)
+
+    val idMapSet_notRelated: List[IdMapSet] = reducedIDMap_DS.filterNot(_.flag).map(_.resetFlag)
+    println("\nNoRelated: " + idMapSet_notRelated.mkString("\n"))
+    val related_idMapSet: List[IdMapSet] = reducedIDMap_DS.filter(_.flag).map(_.resetFlag)
+
+    (Some(idMapSet_notRelated), Some(related_idMapSet))
+  }
+
 
   private def mergeIdMapSetByKey(flattenDataset: List[(String, IdMapSet)]): Map[String, IdMapSet] = {
     val groupedDataSet: Map[String, IdMapSet] = flattenDataset.groupBy(_._1).mapValues{ idMapSet_list: List[(String, IdMapSet)] =>
@@ -41,36 +87,6 @@ object ReduceIDSetApp {
     groupedDataSet
   }
 
-  def reduceIDMapSet(idMapRdd: List[IdMapSet]): (Option[List[IdMapSet]], Option[List[IdMapSet]]) = {
-    // 1. reduce by the wifi
-    val flatten_wf_rdd: List[(String, IdMapSet)] = idMapRdd.flatten { uid_wif_set: IdMapSet =>
-      uid_wif_set.wifiSet.map(wf_k => wf_k -> uid_wif_set)
-    }
-    println("1.1 Flatten Wifi: \n" + flatten_wf_rdd.mkString("\n"))
-    val groupedWf: Map[String, IdMapSet] = mergeIdMapSetByKey(flatten_wf_rdd)
-    printf("1.2 Grouped by WiFi, size %s: \n", groupedWf.size)
-    printf(groupedWf.mkString("\n"))
-
-    // 2. reduce by uid
-    val flatten_uid_rdd: List[(String, IdMapSet)] = groupedWf.values.flatten { uid_wf_map: IdMapSet =>
-      uid_wf_map.uidSet.map(uid_k => uid_k -> uid_wf_map)
-    }.toList
-    println("2.1 Flatten uid: \n" + flatten_uid_rdd.mkString("\n"))
-    val groupedUid: Map[String, IdMapSet] = mergeIdMapSetByKey(flatten_uid_rdd)
-    printf("1.2 Grouped by uid, size %s: \n", groupedUid.size)
-    printf(groupedUid.toList.sortBy(_._1).mkString("\n"))
-
-    val fullJoined_idMapSet: List[IdMapSet] = groupedUid.values.toList
-    // 3. data set not relative to other set
-    val idMapSet_notRelated: List[IdMapSet] = fullJoined_idMapSet.filterNot(_.flag)
-    println("\nNoRelated: " + idMapSet_notRelated.mkString("\n"))
-    val flatten_setId_rdd: List[(String, IdMapSet)] = fullJoined_idMapSet.filter(_.flag).map(idMapSet => idMapSet.setId.toString -> idMapSet)
-    val reducedIdMapSet_list: List[(String, IdMapSet)] = mergeIdMapSetByKey(flatten_setId_rdd).toList
-    printf("3.2 Grouped by setId, size %s: \n", reducedIdMapSet_list.size)
-    printf(reducedIdMapSet_list.sortBy(_._1).mkString("\n"))
-
-    (Some(idMapSet_notRelated.map(_.resetFlag)), Some(reducedIdMapSet_list.map(_._2).map(_.resetFlag)))
-  }
 
 
   def createSetId(): Int = {
